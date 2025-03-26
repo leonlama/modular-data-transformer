ed5e95d334a040e9943d046a58b1e769462c06c1
leonlama <leon.lamarca@yahoo.com>
2025-03-26 12:57:08 +0100

    feat(async): set result backend

--- celery_app.py
+++ celery_app.py
@@ -14,12 +14,14 @@ redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 celery_app = Celery("modular_data_transformer", broker=redis_url, backend=redis_url)
 
 celery_app.conf.update(
+    broker_url=redis_url,
+    result_backend=redis_url,  # make sure the result backend is set
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
     timezone="UTC",
     enable_utc=True,
-    result_expires=3600,  # Results expire after 1 hour (optional)
+    result_expires=3600,  # Results expire after 1 hour
 )
 
 celery_app.autodiscover_tasks(['app.tasks'])
